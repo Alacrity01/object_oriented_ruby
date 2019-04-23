320d2a7d746db1eaa8e8d88588950342044a9bdb
TheAlacrity <brinker.jeff@gmail.com>
2019-04-23 11:27:53 -0500

    added file, entered hashes

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item_1 = {description: "balloons", color: "red", quantity: 100, price: 3.00}
+item_2 = {:description => "silly string", :color => "neon pink", :quantity => 3, :price => 5.00}
+item_3 = {description: "face paint", color: "blue", quantity: 1, price: 4}
