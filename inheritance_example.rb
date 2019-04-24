f541eeb827494cb69a4b1d83a7af0ff60f9328d2
TheAlacrity <brinker.jeff@gmail.com>
2019-04-24 11:12:55 -0500

    improved classes of inheritance_example

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -21,11 +21,11 @@ end
 
 
 class Car < Vehicle
-  def initialize
-    super
-    @fuel = "gasoline"
-    @make = "Honda"
-    @model = "Accord"
+  def initialize(input_options)
+    super()
+    @make = input_options[:make]
+    @fuel = input_options[:fuel]    
+    @model = input_options[:model]
   end
 
   def honk_horn
@@ -34,11 +34,11 @@ class Car < Vehicle
 end
 
 class Bike < Vehicle
-  def initialize
-    super
-    @type = "Mountain bike"
-    @weight = 8
-    @gears = 21
+  def initialize(input_options)
+    super()
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+    @gears = input_options[:gears]
   end
 
   def ring_bell
@@ -46,8 +46,19 @@ class Bike < Vehicle
   end
 end
 
-car_1 = Car.new
-bike_1 = Bike.new
+car_1 = Car.new(
+                make: "Mini",
+                model: "Cooper S",
+                fuel: 10
+                )
+
+bike_1 = Bike.new(
+                  type: "Mountain",
+                  gears: 16,
+                  weight: 15
+                  )
+
+vehicle = Vehicle.new
 
 car_1.accelerate
 bike_1.accelerate
@@ -60,3 +71,4 @@ bike_1.turn("east")
 
 p car_1
 p bike_1
+p vehicle
